0f90e3c7aba73d284ab0074b942307eab0f60560
LeonamMiiller <leonam.miiller@gmail.com>
2024-06-17 13:22:25 -0300

    add read numbers from bytes

--- lib/src/class/access_stream_file.dart
+++ lib/src/class/access_stream_file.dart
@@ -20,11 +20,32 @@ abstract interface class _RamdomAccessStreamInterface {
       {bool findOnyFirst = false, int start = 0, int end = 0});
   int findFirstSublistInitialPosition(Uint8List subListToFind,
       {int start = 0, int end = 0});
+
+  int readInt64([Endian? endian]);
+
+  int readUint64([Endian? endian]);
+
+  int readInt32([Endian? endian]);
+
+  int readUint32([Endian? endian]);
+
+  int readInt16([Endian? endian]);
+
+  int readUint16([Endian? endian]);
+
+  int get readInt8;
+
+  int get readUint8;
+
+  double readFloat32([Endian? endian]);
+
+  double readFloat64([Endian? endian]);
 }
 
 class _RamdomAccessStream implements _RamdomAccessStreamInterface {
   int _offset = 0;
   Uint8List _listOfBytes;
+  static final Endian _endian = getEndian;
 
   _RamdomAccessStream({required Uint8List bytes}) : _listOfBytes = bytes;
 
@@ -118,6 +139,60 @@ class _RamdomAccessStream implements _RamdomAccessStreamInterface {
             .takeBytes();
         break;
     }
+  }
+
+  @override
+  int readInt64([Endian? endian]) {
+    return _readByteData(8).getInt64(0, endian ?? _endian);
+  }
+
+  @override
+  int readUint64([Endian? endian]) {
+    return _readByteData(8).getUint64(0, endian ?? _endian);
+  }
+
+  @override
+  int readInt32([Endian? endian]) {
+    return _readByteData(4).getInt32(0, endian ?? _endian);
+  }
+
+  @override
+  int readUint32([Endian? endian]) {
+    return _readByteData(4).getUint32(0, endian ?? _endian);
+  }
+
+  @override
+  int readInt16([Endian? endian]) {
+    return _readByteData(2).getInt16(0, endian ?? _endian);
+  }
+
+  @override
+  int readUint16([Endian? endian]) {
+    return _readByteData(2).getUint16(0, endian ?? _endian);
+  }
+
+  @override
+  int get readInt8 {
+    return _readByteData(1).getInt8(0);
+  }
+
+  @override
+  int get readUint8 {
+    return _readByteData(1).getUint8(0);
+  }
+
+  @override
+  double readFloat32([Endian? endian]) {
+    return _readByteData(4).getFloat32(0, endian ?? _endian);
+  }
+
+  @override
+  double readFloat64([Endian? endian]) {
+    return _readByteData(4).getFloat64(0, endian ?? _endian);
+  }
+
+  ByteData _readByteData(int lengthOfBytes) {
+    return readSync(lengthOfBytes).buffer.asByteData();
   }
 }
 
